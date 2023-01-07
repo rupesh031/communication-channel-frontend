5369892dc42cab4832497ef7896295fd17196174
Rupesh Yadav <rupeshyadav8178@gmail.com>
2023-01-07 15:48:15 +0530

    minor changes

--- src/useConfig.js
+++ src/useConfig.js
@@ -1,5 +1,5 @@
 // export const api = "https://communication-channel-backend.herokuapp.com/api/";
 // export const socket_api = "https://communication-channel-backend.herokuapp.com/"
 
-export const api = "http://localhost:8080/api/";
-export const socket_api = "http://localhost:8080/"
+export const api = "https://zine-backend.onrender.com/api/";
+export const socket_api = "https://zine-backend.onrender.com/";
